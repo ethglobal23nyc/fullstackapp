72e41eb055d3921329fa24420d94a0d9df776cae
Jason Sun <sunapi386@gmail.com>
2023-09-23 13:32:34 -0400

    generate contracts

---
+++ src/contracts/components/ContractForm.tsx
@@ -0,0 +1,53 @@
+import React, { Suspense } from "react"
+import { Form, FormProps } from "src/core/components/Form"
+import { LabeledTextField } from "src/core/components/LabeledTextField"
+
+import { z } from "zod"
+export { FORM_ERROR } from "src/core/components/Form"
+
+export function ContractForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
+  return (
+    <Form<S> {...props}>
+      <LabeledTextField name="name" label="Name" placeholder="Name" type="text" />
+      <LabeledTextField
+        name="denomination"
+        label="Denomination"
+        placeholder="Denomination"
+        type="number"
+      />
+      <LabeledTextField
+        name="organizationId"
+        label="Organization Id"
+        placeholder="Organization Id"
+        type="number"
+      />
+      <LabeledTextField name="name" label="Name" placeholder="Name" type="text" />
+      <LabeledTextField
+        name="denomination"
+        label="Denomination"
+        placeholder="Denomination"
+        type="number"
+      />
+      <LabeledTextField
+        name="organizationId"
+        label="Organization Id"
+        placeholder="Organization Id"
+        type="number"
+      />
+      <LabeledTextField name="name" label="Name" placeholder="Name" type="text" />
+      <LabeledTextField
+        name="denomination"
+        label="Denomination"
+        placeholder="Denomination"
+        type="number"
+      />
+      <LabeledTextField
+        name="organizationId"
+        label="Organization Id"
+        placeholder="Organization Id"
+        type="number"
+      />
+      {/* template: <__component__ name="__fieldName__" label="__Field_Name__" placeholder="__Field_Name__"  type="__inputType__" /> */}
+    </Form>
+  )
+}
---
+++ src/contracts/schemas.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+
+export const CreateContractSchema = z.object({
+  name: z.string(),
+  denomination: z.number(),
+  organizationId: z.number(),
+  name: z.string(),
+  denomination: z.number(),
+  organizationId: z.number(),
+  name: z.string(),
+  denomination: z.number(),
+  organizationId: z.number(),
+  // template: __fieldName__: z.__zodType__(),
+})
+export const UpdateContractSchema = CreateContractSchema.merge(
+  z.object({
+    id: z.number(),
+  })
+)
+
+export const DeleteContractSchema = z.object({
+  id: z.number(),
+})
